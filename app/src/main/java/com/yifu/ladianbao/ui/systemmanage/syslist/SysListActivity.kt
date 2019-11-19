89e6b88accef3a5bcf6654290fca72416922d00e
maqc666 <888222y@sina.com>
2019-11-19 09:06:40 +0800

    19/11/19  完成主界面

---
+++ app/src/main/java/com/yifu/ladianbao/ui/systemmanage/syslist/SysListActivity.kt
@@ -0,0 +1,13 @@
+package com.yifu.ladianbao.ui.systemmanage.syslist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yifu.ladianbao.R
+
+class SysListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sys_list)
+    }
+}
